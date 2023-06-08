a4507cd33a76d52bc58c72cae4483485ce6b2c3f
Bishoy P <bishoypramanik@gmail.com>
2023-06-08 17:10:29 -0400

    this shit aint building wtf

--- avl/src/include/avl.h
+++ avl/src/include/avl.h
@@ -15,9 +15,9 @@ struct AVLTree{
         std::unique_ptr<std::string> name;
         std::unique_ptr<int> gator_id;
 
-        Node(const std::string name, const int id, std::shared_ptr<Node> p=nullptr, std::shared_ptr<Node> l=nullptr , std::shared_ptr<Node> r=nullptr)
+        Node(const std::string name, const int id, std::shared_ptr<Node> p=nullptr,
+                 std::shared_ptr<Node> l=nullptr , std::shared_ptr<Node> r=nullptr)
         {
-
             this->left = l;
             this->right = r;
             this->parent = p;
@@ -39,6 +39,17 @@ struct AVLTree{
     std::unique_ptr<Node> root;
 
     /**
+     * @brief Construct a new AVLTree object
+     * 
+     * @param name Initial Student Name for the root node
+     * @param id Initial 8-Digit ID for the root node
+     */
+    AVLTree(const std::string name, const int id)
+    {
+        this->root = std::make_unique<Node>(name, id);
+    }
+
+    /**
      * @brief 
      * Add a Student object into the tree with the specified name, NAME and GatorID number, ID.
      * Balance the tree automatically if necessary.
@@ -58,7 +69,9 @@ struct AVLTree{
     /**
      * @brief 
      * Find and remove the account with the specified ID from the tree.
-     * [Optional: Balance the tree automatically if necessary. We will test your code only on cases where the tree will be balanced before and after the deletion. So you can implement a BST deletion and still get full credit]
+     * [Optional: Balance the tree automatically if necessary. 
+     * We will test your code only on cases where the tree will be balanced before and after the deletion. 
+     * So you can implement a BST deletion and still get full credit]
      * If deletion is successful, print “successful”.
      * If the ID does not exist within the tree, print “unsuccessful”.
      * You must prioritize replacing a removed node with its inorder successor for the case where the deleted node has two children.
@@ -75,7 +88,6 @@ struct AVLTree{
      * Search for the student with the specified ID from the tree.
      * If the ID was found, print out their NAME.
      * If the ID does not exist within the tree, print “unsuccessful”.
-     * 
      * @param id Unique 8-Digit ID number
      */
     void search(const int id)
@@ -87,7 +99,8 @@ struct AVLTree{
      * @brief 
      * Search for the student with the specified name, NAME in the tree.
      * If the student name was found, print the associated ID.
-     * If the tree has more than one object with the same NAME, print each ID on a new line with no other spaces and in the same relative order as a pre-order traversal.
+     * If the tree has more than one object with the same NAME, 
+     * thenprint each ID on a new line with no other spaces and in the same relative order as a pre-order traversal.
      * If the name does not exist within the tree, print “unsuccessful”.
      * NAME identifier will be surrounded by double quotes for parsing, e.g. "Josh Smith".
      * 
@@ -132,7 +145,9 @@ struct AVLTree{
      * @brief 
      * Remove the Nth GatorID from the inorder traversal of the tree (N = 0 for the first item, etc).
      * If removal is successful, print “successful”.
-     * [Optional: Balance the tree automatically if necessary. We will test your code only on cases where the tree will be balanced before and after the deletion. So you can implement a BST deletion and still get full credit]
+     * [Optional: Balance the tree automatically if necessary. 
+     * We will test your code only on cases where the tree will be balanced before and after the deletion. 
+     * So you can implement a BST deletion and still get full credit]
      * If the Nth GatorID does not exist within the tree, print “unsuccessful”.
      * @param n Nth Gator-ID from the IN-ORDER traversal
      */
--- avl/tests/avl_test.cpp
+++ avl/tests/avl_test.cpp
@@ -1,2 +1,16 @@
 #include "../src/include/avl.h"
 #include <iostream>
+#include <gtest/gtest.h>
+
+TEST(Basic, Initialization)
+{
+    AVLTree avl = AVLTree::AVLTree("Bishoy", 12345678);
+
+    std::cout << avl.root->name << ", " << avl.root->gator_id ; 
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
