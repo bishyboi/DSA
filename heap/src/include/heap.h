55552faf9548c3fb40f906ea25b89da7b8ad8ffa
Bishoy P <bishoypramanik@gmail.com>
2023-07-09 11:22:59 -0400

    init commit for heap, idk why cmake:build isnt working through vsc, but is through terminal ill figure it out later

---
+++ heap/src/include/heap.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+int* extractMax(int arr[], int size)
+{
+    // code here
+	// main prints the final heap array from 0 to size - 2
+	// you don't need to print the array but instead return
+	return arr; 
+}
---
+++ heap/tests/heap_test.cpp
@@ -0,0 +1,13 @@
+#include "../src/include/heap.h"
+#include <gtest/gtest.h>
+
+TEST(Basic, 1)
+{
+    EXPECT_EQ(1,1);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
