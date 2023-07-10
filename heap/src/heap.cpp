bd9604422ce0c0b585d20c681a0c42aac8e27311
Bishoy P <bishoypramanik@gmail.com>
2023-07-09 23:01:35 -0400

    should be fine lol

--- heap/src/heap.cpp
+++ heap/src/heap.cpp
@@ -1,8 +1,15 @@
 #include "include/heap.h"
-//#include <array>
+
 int main()
 {
     int arr[] = {11, 5, 8, 3, 4};
-
+    int arr_eq[] = {8, 5, 4, 3};
     
+    std::string arr_s = arrstr(arr, std::size(arr));
+    std::string eq_s = arrstr(arr_eq, std::size(arr_eq));
+    std::string in_s = arrstr(extractMax(arr, std::size(arr)), std::size(arr));
+
+    std::cout<< "Input: " << arr_s << std::endl;
+    std::cout<< "Output: " << in_s << std::endl;
+    std::cout<< "Expected Output: "<<  eq_s << std::endl;
 }
--- heap/tests/heap_test.cpp
+++ heap/tests/heap_test.cpp
@@ -3,8 +3,6 @@
 
 int arr[] = {11, 5, 8, 3, 4};
 
-
-
 TEST(Basic, get_left)
 {
     EXPECT_EQ(arr[get_left(0)], 5);
@@ -23,8 +21,43 @@ TEST(Basic, get_parent)
 
 TEST(extract, base)
 {
-    int arr_eq = {8, 5, 4, 3};
-    EXPECT_EQ(extractMax(arr, std::size(arr)), arr_eq);
+    int arr_eq[] = {8, 5, 4, 3};
+
+    std::string arr_s = arrstr(extractMax(arr, std::size(arr)), std::size(arr));
+    std::string eq_s = arrstr(arr_eq, std::size(arr_eq));
+    EXPECT_EQ(arr_s, eq_s);
+}
+
+TEST(extract, 2elem)
+{
+    int arr[] = {3, 1};
+    int arr_eq[] = {1};
+
+    std::string arr_s = arrstr(extractMax(arr, std::size(arr)), std::size(arr));
+    std::string eq_s = arrstr(arr_eq, std::size(arr_eq));
+    EXPECT_EQ(arr_s, eq_s);
+}
+
+TEST(extract, multipleSwaps)
+{
+    int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int arr_eq[] = {9, 7, 8, 3, 6, 5, 4, 1, 2};
+
+    std::string arr_s = arrstr(extractMax(arr, std::size(arr)), std::size(arr));
+    std::string eq_s = arrstr(arr_eq, std::size(arr_eq));
+    EXPECT_EQ(arr_s, eq_s);
+}
+
+TEST(extract, repeatDeleteHell)
+{
+    int arr[] = {323, 252, 282, 121, 131, 222, 141, 66, 111, 85, 50, 182, 107, 64, 91, 15, 20};
+    int arr_eq[] = {182, 131, 141, 121, 85, 107, 64, 66, 111, 15, 50, 70, 91};
+    for (int i = 0; i < 4; i++)
+        extractMax(arr, std::size(arr));
+
+    std::string arr_s = arrstr(arr, std::size(arr));
+    std::string eq_s = arrstr(arr_eq, std::size(arr_eq));
+    EXPECT_EQ(arr_s, eq_s);
 }
 
 int main(int argc, char **argv)
